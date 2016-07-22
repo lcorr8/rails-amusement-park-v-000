36292a19020404dfa01cd575f6c17379e14c6f2c
lcorr8 <l.corr.v@gmail.com>
2016-07-22 10:49:07 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_secure_password
+  # validate :password
+  #defaults to admin => false  
+
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood 
+
+  end
+
 end
